f6a71eca96dac39d6d682ef66a5af8277a0f48ef
Ahmadshawawreh-99 <ahmed.akalshwaorha@gmail.com>
2025-04-21 20:49:02 +0300

    update

---
+++ main.dart
@@ -0,0 +1,18 @@
+void main(){
+
+String myname ="ahmed";
+
+int myage= 26;
+
+double myhightest =171.5;  
+print(myname[0].toUpperCase());
+
+print(myage);
+
+print(myhightest.ceil());
+print(myhightest.floor());
+print(myhightest.round());
+print(myhightest.isNegative);
+print(myhightest.abs());
+
+}
