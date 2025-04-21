313656a1d96800148ce4e2b6b8e7832aac0488d5
Ahmadshawawreh-99 <ahmed.akalshwaorha@gmail.com>
2025-04-21 20:57:27 +0300

    update

--- main.dart
+++ main.dart
@@ -3,6 +3,7 @@ void main(){
 String myname ="ahmed";
 
 int myage= 26;
+int myyear=1999;
 
 double myhightest =171.5;  
 print(myname[0].toUpperCase());
@@ -14,5 +15,6 @@ print(myhightest.floor());
 print(myhightest.round());
 print(myhightest.isNegative);
 print(myhightest.abs());
-
+int myageafter10year= myage+10;
+print("my age after 10 years is $myageafter10year");
 }
